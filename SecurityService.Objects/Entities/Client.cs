e45b33b27b69c67ef914ea311814967b478a8834
Mike <mtastle@hotmail.com>
2020-11-12 13:33:17 +0000

    Added more structure
    Added Objects and Data projects and a couple of entities

---
+++ SecurityService.Objects/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SecurityService.Objects.Entities
+{
+    /// <summary>
+    /// An entitiy for the callers of the API
+    /// </summary>
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SecurityService.Objects/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityService.Objects.Entities
+{
+    /// <summary>
+    /// An entity for the users who are stored in the API. Each user belongs to a Client
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string UserName { get; set; }
+    }
+}
